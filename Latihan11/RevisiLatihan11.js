af114dc1c2e93b80de64d1b4756da2f53aa83c84
sony2903 <36514037+sony2903@users.noreply.github.com>
2020-03-01 11:25:59 +0700

    Add files via upload

---
+++ Latihan11/RevisiLatihan11.js
@@ -0,0 +1,21 @@
+// Diberikan sebuah function balikKata(kata) yang menerima satu parameter berupa string. 
+// Function akan me-return kata yang dibalik. 
+// Contoh, jika kata adalah "John Doe", function akan me-return "eoD nhoJ".
+
+function balikKata(kata) {
+    // you can only write your code here!
+    var i,kataLength, katabalik
+    awalan = 0
+    katabalik = ''
+    kataLength = kata.length
+    for(i = kataLength - 1; i >= 0; i--){
+        katabalik += kata[i]
+    } return katabalik
+  }
+ 
+  // TEST CASES
+  console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
+  console.log(balikKata('John Doe')); // eoD nhoJ
+  console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
+  console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
+  console.log(balikKata('Super')); // repuS
